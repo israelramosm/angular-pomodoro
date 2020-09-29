24fdc16fe223978ce3a9231db5ba195ca16bd073
Israel Ramos <arnulfo_israel@hotmail.com>
2020-09-29 00:21:43 -0500

    moving pomodoro componentn to app.clock component

---
+++ my-pomodoro/src/app/app-clock/app.clock.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-clock',
+  templateUrl: './app.clock.html',
+  styleUrls: ['./app.clock.css']
+})
+export class AppClock {
+      
+    constructor() {}
+
+
+    startTimer() {}
+
+    stopTimer() {}
+
+    resetTimer() {}
+}
