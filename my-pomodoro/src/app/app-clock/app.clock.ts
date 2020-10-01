c6dd8b3364d3a8c71f842cc2231a9a301edfb5ed
Israel Ramos <arnulfo_israel@hotmail.com>
2020-09-30 22:17:39 -0500

    start pause logic working

--- my-pomodoro/src/app/app-clock/app.clock.ts
+++ my-pomodoro/src/app/app-clock/app.clock.ts
@@ -6,13 +6,72 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./app.clock.css']
 })
 export class AppClock {
-    @Input() setConfig = {};
+  @Input() setConfig;
 
-    constructor() {}
+  intervalBreaks;
+  mmLeft;
+  ssLeft;
+  timer;
+  isTimerPause = true;
 
-    startTimer() {}
 
-    stopTimer() {}
+  constructor() { }
 
-    resetTimer() {}
+  ngOnInit() {
+    this.setConfig = {
+      pomodoroName: "",
+      startTime: 25,
+      shortBreakTime: 5,
+      longBreakTime: 20,
+      breakInterval: 4,
+      isDone: false
+    }
+    this.mmLeft = this.setConfig.startTime;
+    this.ssLeft = "00";
+    this.intervalBreaks = this.setConfig.breakInterval;
+  }
+
+  ngOnChanges(changes) {
+    this.setConfig = changes.setConfig.currentValue;
+    this.mmLeft = this.setConfig.startTime;
+  }
+
+  startPauseTimer() {
+    if(this.isTimerPause) {
+      this.isTimerPause = false;
+      this.timer = setInterval(() => {
+        this.timerLogic();
+      }, 1000);
+    } else {
+      clearInterval(this.timer);
+      this.isTimerPause = true;
+    }
+    
+  }
+
+  stopTimer() {
+    this.mmLeft = this.setConfig.startTime;
+    this.ssLeft = "00";
+    this.isTimerPause = true;
+    clearInterval(this.timer);
+  }
+
+  timerLogic() {
+    if (this.mmLeft >= 0) {
+      if(this.ssLeft == "00") {
+        this.ssLeft = 59;
+        this.mmLeft--;
+        this.mmLeft = this.mmLeft < 10 ? "0" + this.mmLeft-- : this.mmLeft;
+      } else {
+        this.ssLeft--;
+        this.ssLeft = this.ssLeft < 10 ? "0" + this.ssLeft-- : this.ssLeft;
+      }
+    } else {
+      if(this.intervalBreaks > 0){
+        this.intervalBreaks--;
+        this.mmLeft = this.setConfig.shortBreakTime;
+        this.startPauseTimer();
+      }
+    }
+  }
 }
